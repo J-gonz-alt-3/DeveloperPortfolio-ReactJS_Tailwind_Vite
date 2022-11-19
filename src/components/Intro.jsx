49a9ca61a253b78233090c94a9c2422600c36171
jeanpierre <jean.gonzales1414@gmail.com>
2022-11-18 22:56:39 -0500

    First commit

---
+++ src/components/Intro.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Intro = () => {
+  return (
+    <div className="flex item-center justify-center flex-col text-center pt-20 pb-6">
+      Intro
+    </div>
+  );
+};
+
+export default Intro;
