02a38d14d1916607e5a715ebbe0702123e3cbddb
jeanpierre <jean.gonzales1414@gmail.com>
2022-11-23 00:10:28 -0500

    final changes of the night to live portfolio

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { AiFillLinkedin } from "react-icons/ai";
+import Title from "./Title";
 
 const Intro = () => {
   return (
@@ -7,15 +9,43 @@ const Intro = () => {
         JeanPierre Gonzales-Altamirano
       </h1>
       <p className="text-base md:text-xl mb-3 font-medium">Web Developer</p>
+      <div className="text-5xl flex justify-center py-3 text-blue-600">
+        <a
+          href="https://www.linkedin.com/in/gonzalesaltamiranoje/"
+          target="_blank"
+        >
+          <AiFillLinkedin />
+        </a>
+      </div>
       <p className="text-sm max-w-xl mb-6 font-bold">
         I'm an indie Front-End Web Developer, who is working towards becoming a
-        Full Stack Web Developer in the near future! All coding projects are
-        built from the ground up! I do take design inspiration from sites such
-        as Figma, however all source code is written by me!
+        Full Stack Web Developer in the near future! <br />
         <br />I am 100% self-taught, with most of my foundational training
-        coming from completing projects on FreeCodeCamp.com as well as just
-        watching hours upon hours of YouTube videos on Web Development topics!
+        coming from completing countless projects and even receiving the{" "}
+        <span className="text-teal-500">
+          <a
+            href="https://www.freecodecamp.org/certification/jpgcodes/responsive-web-design"
+            target="_blank"
+          >
+            Responsive Web Design Certification
+          </a>
+        </span>{" "}
+        from{" "}
+        <span className="text-teal-500">
+          <a href="https://www.freecodecamp.org" target="_blank">
+            freecodecamp.org
+          </a>
+        </span>
+        . <br />
+        <br />
+        Aside from that, I've also dedicated countless hours to watching various
+        YouTube channels/videos on Web Development topics!
+        <br />
+        <br />
+        All coding projects below have been built from the ground up using
+        VSCode!
       </p>
+      <Title>Completed Projects</Title>
     </div>
   );
 };
