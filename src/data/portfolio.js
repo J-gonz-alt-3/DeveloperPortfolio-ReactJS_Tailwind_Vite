251df548cfc050e814ea0f22d280e8b20d3143f3
jeanpierre <jean.gonzales1414@gmail.com>
2022-11-22 23:37:18 -0500

    more changes to live minimal portfolio

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,9 +1,15 @@
 export default [
   {
     title: "Developer Portfolio",
-    imgUrl: "/assets/portfolio.jpg",
+    imgUrl: "/assets/portfolio2.jpg",
     stack: ["React", "Tailwind", "Vite"],
     link: "https://jpgcodes.com",
+  },
+  {
+    title: "Alternative Developer Portfolio",
+    imgUrl: "/assets/portfolio_v2.jpg",
+    stack: ["NextJS", "Tailwind", "JavaScript"],
+    link: "https://portfolio-beta-react-tailwind-next-js.vercel.app",
   },
   {
     title: "Modern Restaurant Web App",
@@ -16,11 +22,5 @@ export default [
     imgUrl: "/assets/ai_site.jpg",
     stack: ["React", "CSS", "JavaScript"],
     link: "https://gpt3-jpg.com",
-  },
-  {
-    title: "Alternative Developer Portfolio",
-    imgUrl: "/assets/portfolio_v2.jpg",
-    stack: ["NextJS", "Tailwind", "JavaScript"],
-    link: "https://portfolio-beta-react-tailwind-next-js.vercel.app",
   },
 ];
