15325c59301d76f4d46bc6e78c6b50ba47dc99c4
jeanpierre <jean.gonzales1414@gmail.com>
2022-12-21 16:49:13 -0500

    adding new additions to portfolio

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -4,7 +4,7 @@ import { AiFillLinkedin } from "react-icons/ai";
 const Intro = () => {
   return (
     <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
-      <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">
+      <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold font-serif">
         JeanPierre <br />
         Gonzales-Altamirano
       </h1>
