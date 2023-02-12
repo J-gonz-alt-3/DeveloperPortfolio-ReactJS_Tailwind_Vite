57e1f9b59ca4975cb4e7c4f64fc9ebbbf5854097
jeanpierre <jean.gonzales1414@gmail.com>
2023-02-12 14:30:38 -0500

    adding new info

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -5,7 +5,7 @@ export default [
     company: "Identity Automation",
     duration: "1 month",
     details:
-      "I write Java 'Action Sets' (code blocks) using the company's in house IDE to pull, transform and move data from client sources (Active Directory, XML files, txt files, etc.) to other applications.",
+      "I write Java 'Action Sets' (Java Functions) using the company's in house IDE (Connect) to pull, transform and move data from client sources (Active Directory, XML files, txt files, etc.) to other applications.",
   },
   {
     year: "2023",
@@ -21,7 +21,7 @@ export default [
     company: "Freelance",
     duration: "4 months",
     details:
-      "I designed Responsive Web Applications for start-up clients in CT, utilizing Frontend technologies such as ReactJS, NextJS, TailwindCSS, Vite, and GitHub! Design, development, and deployment were all planned and completed by me within agreed upon timeframe",
+      "I designed/coded Responsive Web Applications for start-up clients in CT, utilizing Frontend technologies such as ReactJS, NextJS, TailwindCSS, Vite, and GitHub for version control. All projects deliverables were discussed and delivered to the client(s) throughout project lifecycle and only brought onto live servers once client was satisfied with the product.",
   },
   {
     year: "2022",
@@ -29,7 +29,7 @@ export default [
     company: "Tunxis Community College",
     duration: "7 months",
     details:
-      "I taught Python Programming for 2 semesters and Windows Server Administration for 1 semester. For the Python courses, I focused primarily on teaching the students program Data Structures, some common coding Algorithms, and Problem Solving to solve coding problems. Students enjoyed my teaching approach so much that many of them ended up switching their majors to Computer Science (which they told me at the end of the semester)!",
+      "I taught Python Programming (for 2 semesters) and Windows Server Administration (for 1 semester). For the Python courses, I focused primarily on teaching the students Python Data Structures, common coding Algorithms, and Problem Solving techniques to solve coding problems. Students enjoyed my teaching approach so much that many of them ended up switching their majors to Computer Science (which they told me at the end of the semester)!",
   },
   {
     year: "2022",
@@ -37,7 +37,7 @@ export default [
     company: "Gateway Community College",
     duration: "5 months",
     details:
-      "I taught Structured Programming: Engineering Problem Solving with C++. I focused primarily on teaching the students program Data Structures, Algorithms, and Problem Solving approaches to solve common 'Easy-Medium' LeetCode problems as well as Engineering problems (utilizing common mathematical formulas).",
+      "I taught Structured Programming: Engineering Problem Solving with C++. I focused primarily on teaching the students C++ Data Structures, common coding Algorithms, and Problem Solving techniques to solve common 'Easy-Medium' LeetCode problems as well as Engineering problems (utilizing common mathematical formulas).",
   },
   {
     year: "2020-2022",
@@ -45,7 +45,7 @@ export default [
     company: "University of Maryland Global Campus",
     duration: "2 years",
     details:
-      "It was around this time that COVID-19 and personal health issues forced me to re-evaluate my current life direction. After some consideration, I decided to continue my formal education by enrolling at the University of Maryland Global Campus to further enhance my knowledge-base in the field of Cybersecurity (Masters Degree). It was here that I was able to obtain a better understanding of Security Frameworks provided by bodies such NIST and others. I was also able to understand the difficulties that arise with the rise/use of Crypto currency, the struggles of jurisdiction when it comes to  international cyberlaw, among many other topics!",
+      "Due to COVID-19 and personal health issues, I took time off from my professional career and decided to continue my formal education by enrolling at UMGC to further enhance my knowledge-base in the field of Cybersecurity. It was here that I was able to obtain a better understanding of Cybersecurity Frameworks provided by public entities such as NIST. I was also able to better understand the role of cryptography with regards to Cryptocurrency, and the struggles of jurisdiction when it comes to  international cyberlaw, among many other topics!",
   },
   {
     year: "2019-2020",
@@ -53,7 +53,7 @@ export default [
     company: "SUND Technologies",
     duration: "5 months",
     details:
-      "I was tasked with the management of 3 medium sized client companies’ IT network infrastructures. This included using VMWare vCenter to manage Linux and Windows Servers.Microsoft Azure Active Directory to manage hybrid client environments. I was also in charge of managing client Data Backups via Datto’s cloud console manager, as well as client Firewalls via VPN remote access. Finally, client WLANs were also managed by me, as well as overall network troubleshooting 24/7 (I was always on call).",
+      "Tasked with the management of 3 medium sized client companies’ IT network infrastructures. This included using VMWare vCenter to manage Linux and Windows Servers. Microsoft Azure Active Directory to manage hybrid client environments (not all my clients used this, but 1 did). I was in charge of managing client Data Backups via Datto’s cloud console manager. I also managed client Firewalls, and provided secure remote connections for users. Finally, client WLANs were also managed by me, as well as overall network troubleshooting 24/7 (always on-call).",
   },
   {
     year: "2019",
@@ -61,7 +61,7 @@ export default [
     company: "Orange Public Schools",
     duration: "6 months",
     details:
-      "I implemented the districts first WDS server to help streamline staff/student PC/laptop image deployment. I also wrote Bash and PowerShell scripts, which I included within the WDS server configuration to help automate certain customizations for specific software/Windows settings. I assisted in the management of district Active Directory, PowerSchool, Google Admin Console, and MDM Solution (JAMF). I also assisted the Network Manager in configuring and installing district L3 Juniper, and Cisco Switches. Finally, I provided in person/remote technical assistance to district staff.",
+      "I implemented the district's first WDS server to help streamline staff/student PC/laptop image deployment. I also wrote Bash and PowerShell scripts, which I included within the WDS server configuration to help automate certain customizations for specific software/Windows settings. I assisted in the management of district Active Directory, PowerSchool, Google Admin Console, and MDM Solution (JAMF). I also assisted the Network Manager in configuring and installing district L3 Juniper, and Cisco Switches. Finally, I provided in person/remote technical assistance to district staff.",
   },
   {
     year: "2018",
@@ -69,7 +69,7 @@ export default [
     company: "Colchester Public Schools",
     duration: "10 months",
     details:
-      "I was assigned as the Lead Admin for the district's MDM solution TechPilot, which managed over 800 active IoT devices. I was also responsible for the Management of two Level I technicians, and was in charge of assigning them daily tasks and rating their overall performance to the IT Director on a weekly basis. I assisted in the management of district Active Directory, PowerSchool, and Google Admin Console. I also assisted the Network Manager with the configuration and deployment of district Cisco L3 Switches. Finally, I provided in person/remote technical assistance to district staff.",
+      "I was assigned as the Lead Admin for the district's MDM solution TechPilot, which managed over 800 active IoT devices. I was also responsible for the management of two Level I technicians, and was in charge of assigning them daily tasks and rating their overall performance to the IT Director on a weekly basis. I assisted in the management of district Active Directory, PowerSchool, and Google Admin Console. I also assisted the Network Manager with the configuration and deployment of district Cisco L3 Switches. Finally, I provided in person/remote technical assistance to district staff.",
   },
   {
     year: "2014-2018",
@@ -77,6 +77,6 @@ export default [
     company: "St. John's University",
     duration: "4 Years",
     details:
-      "I pursued the knowledge that a Cyber Security Systems degree had to offer. I learned about Coding Data Structures, SQL Databases, Linux, Networking, Digital Forensics, and of course Cyber Security theory during my time at St. John's University.",
+      "I pursued the knowledge that a Cyber Security Systems degree had to offer. I learned about Coding Data Structures, SQL Databases, Linux, Networking, Digital Forensics, and of course Cyber Security theory/hands-on practice during my time at St. John's University.",
   },
 ];
