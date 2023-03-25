9a286d972a5d0ce0c0e472326e452492112298e6
jeanpierre <jean.gonzales1414@gmail.com>
2023-03-25 09:34:13 -0400

    adding new info

---
+++ src/data/timeline.jsx
@@ -0,0 +1,233 @@
+export default [
+  {
+    year: "2023",
+    title: "Support Engineer",
+    company: "Identity Automation",
+    duration: "3 months",
+    details: (
+      <ul>
+        <li>
+          {" "}
+          - I write Java programs using the company IDE's (Connect) to access
+          data from client local systems which I then read, transform, and
+          output to other end systems (typically cloud tenants) for better data
+          management/accessibility.
+        </li>
+        <li>
+          - I also provide support for client RI on-prem and cloud tenants
+          (which run on CentOS 7).
+        </li>
+        <li>
+          - When testing possible fixes, to ensure quality solutions/minimal
+          client downtime, fixes are first tested on my own RI (on-prem) virtual
+          environment (which runs on UTM hypervisor) before being pushed to
+          client environment.
+        </li>
+        <li>
+          {" "}
+          - I provide management of client Active Directory and LDAP through the
+          use of LDAP/Active Directory filters, which I incorporate into my
+          Connect (Java) programs and execute any changes to those environments
+          through the Java program execution.
+        </li>
+      </ul>
+    ),
+  },
+  {
+    year: "2023",
+    title: "Adjunct Professor",
+    company: "Gateway Community College",
+    duration: "3 months",
+    details: (
+      <ol>
+        <li>
+          - Currently assigned to the College's Engineering Department, I
+          currently teach Structured Programming: Engineering Problem Solving
+          with C++.
+        </li>
+        <li>
+          - I mainly focus on teaching students Problem Solving Approaches, C++
+          Data Structures, and common Coding Algorithms.
+        </li>
+        <li>
+          - Teaching approach is to first lecture students for roughly 40-60
+          minutes on the topic assigned for that particular class, after which I
+          then have students do hands-on Coding Problems.
+        </li>
+        <li>
+          - The first 1 or 2 Coding Problems are typically written out on the
+          whiteboard by me, however all of the code is provided by the students
+          (open forum type discourse), where I ask students how we should solve
+          the problem and whenever they get stuck, I provide leading reasoning
+          to help them reach the solution.
+        </li>
+      </ol>
+    ),
+  },
+  {
+    year: "2022",
+    title: "Front-End Web Developer",
+    company: "Freelance",
+    duration: "4 months",
+    details: (
+      <ol>
+        <li>
+          - Responsible for the design/coding process for creating Responsive
+          Web Apps/Websites for startup clients in CT.
+        </li>
+        <li>
+          - Web Apps/Websites varied from single page to multi-page sites and
+          were typically developed using NextJS as a framework.
+        </li>
+        <li>
+          - Aside from NextJS, technologies that were utilized in client
+          projects were TailwindCSS, MongoDB and Git for version control.
+        </li>
+        <li>
+          - Some clients also provided Figma (Design) Files, which assisted me
+          in having to design from scratch.
+        </li>
+      </ol>
+    ),
+  },
+  {
+    year: "2022",
+    title: "Adjunct Professor",
+    company: "Tunxis Community College",
+    duration: "7 months",
+    details: (
+      <ol>
+        <li>
+          - Assigned to the College’s Computer Information Systems Department, I
+          taught Programming Logic & Design w/ Python (for 2 semesters) and
+          Windows Server Administration (for 1 semester).
+        </li>
+        <li>
+          - For the Python courses, I focused primarily on teaching students
+          Python Data Structures, Coding Algorithms, and Problem-Solving
+          Techniques.
+        </li>
+        <li>
+          - For Windows Server Administration, I focused primarily on showing
+          students the various features/tools available in Active Directory, how
+          user management could be done via the GUI but also via PowerShell and
+          the advantages/disadvantages to both approaches.
+        </li>
+      </ol>
+    ),
+  },
+  {
+    year: "2022",
+    title: "Adjunct Professor",
+    company: "Gateway Community College",
+    duration: "5 months",
+    details: (
+      <ol>
+        <li>
+          - Assigned to the College’s Engineering Department, I taught
+          Structured Programming: Engineering Problem Solving with C++. (for 1
+          semester).
+        </li>
+        <li>
+          - My main focus was to teach students Problem Solving Approaches, C++
+          Data Structures, and common Coding Algorithms to help them understand
+          how to solve a wide variety of coding problems.
+        </li>
+        <li>
+          - Lectures were given for the first part of class, and then students
+          were given the rest of the class time to solve the provided Coding
+          Packet Problems (some of which we completed together as a class on the
+          whiteboard).
+        </li>
+      </ol>
+    ),
+  },
+  {
+    year: "2020-2022",
+    title: "M.S. in Cybersecurity Management and Policy",
+    company: "University of Maryland Global Campus",
+    duration: "2 years",
+    details: (
+      <ol>
+        <li>
+          - Due to COVID-19 and personal health issues, I took time off from my
+          professional career and decided to continue my formal education by
+          enrolling at UMGC to further enhance my knowledge-base in the field of
+          Cybersecurity.
+        </li>
+        <li>
+          - It was here that I was able to obtain a better understanding of
+          Cybersecurity Frameworks, such as those provided by NIST.
+        </li>
+        <li>
+          - I was also able to better understand the role of cryptography, and
+          the struggles of jurisdiction when it comes to international cyberlaw,
+          among many other topics!
+        </li>
+      </ol>
+    ),
+  },
+  {
+    year: "2019-2020",
+    title: "Systems Engineer I",
+    company: "SUND Technologies",
+    duration: "5 months",
+    details: (
+      <ol>
+        <li>
+          - Responsible for the management of three small/medium sized client
+          companies’ IT network infrastructure (CT based clients).
+        </li>
+        <li>
+          - Utilized Hypervisor software (vSphere Hypervisor) to manage client
+          Linux (CentOS 7) and Windows Servers.
+        </li>
+        <li>
+          - Managed Microsoft Azure Active Directory for clients who had cloud
+          environments but also had to manage situations in which client’s had
+          hybrid (on-prem and cloud) environments.
+        </li>
+        <li>
+          - Regularly provided maintenance and support for client Data Backups
+          via Datto's cloud console manager.
+        </li>
+        <li>
+          - Clients were using Datto Siris devices which held local and cloud
+          backups of client data.
+        </li>
+        <li>
+          - Setup and maintained client WLANs, this included mapping out best
+          locations for Wireless APs.
+        </li>
+        <li>
+          - Utilized ConnectWise as the company ticketing system to manage
+          client tickets, clock billable hours, and store client documentation.
+        </li>
+      </ol>
+    ),
+  },
+  {
+    year: "2019",
+    title: "Technology Specialist",
+    company: "Orange Public Schools",
+    duration: "6 months",
+    details:
+      "I implemented the district's first WDS server to help streamline staff/student PC/laptop image deployment. I also wrote Bash and PowerShell scripts, which I included within the WDS server configuration to help automate certain customizations for specific software/Windows settings. I assisted in the management of district Active Directory, PowerSchool, Google Admin Console, and MDM Solution (JAMF). I also assisted the Network Manager in configuring and installing district L3 Juniper, and Cisco Switches. Finally, I provided in person/remote technical assistance to district staff.",
+  },
+  {
+    year: "2018",
+    title: "Level II Technician",
+    company: "Colchester Public Schools",
+    duration: "10 months",
+    details:
+      "I was assigned as the Lead Admin for the district's MDM solution TechPilot, which managed over 800 active IoT devices. I was also responsible for the management of two Level I technicians, and was in charge of assigning them daily tasks and rating their overall performance to the IT Director on a weekly basis. I assisted in the management of district Active Directory, PowerSchool, and Google Admin Console. I also assisted the Network Manager with the configuration and deployment of district Cisco L3 Switches. Finally, I provided in person/remote technical assistance to district staff.",
+  },
+  {
+    year: "2014-2018",
+    title: "B.S. in Cyber Security Systems",
+    company: "St. John's University",
+    duration: "4 Years",
+    details:
+      "I pursued the knowledge that a Cyber Security Systems degree had to offer. I learned about Coding Data Structures, SQL Databases, Linux (CentOS), Computer Networking, Digital Forensics, and of course Cyber Security theory/hands-on practice during my time at St. John's University.",
+  },
+];
