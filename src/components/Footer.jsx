948f060188e08ff182b8c0396df52e16ad2d67ef
jeanpierre <jean.gonzales1414@gmail.com>
2023-03-25 11:01:26 -0400

    adding new info

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="py-5 text-center text-sky-700 dark:text-sky-400">
       <p className="text-sm mt-2 opacity-50">
-        &copy; {new Date().getFullYear()} JeanPierre Gonzales-Altamirano. All
+        &copy; {new Date().getFullYear()} Jean Pierre Gonzales Altamirano. All
         Rights Reserved.
       </p>
     </div>
