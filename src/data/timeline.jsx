27535a2d67e1ab252aee253d6955eb6f5a4a511d
jeanpierre <jean.gonzales1414@gmail.com>
2023-04-01 00:28:38 -0400

    Updating Timeline.jsx

--- src/data/timeline.jsx
+++ src/data/timeline.jsx
@@ -48,21 +48,21 @@ export default [
     details: (
       <ol>
         <li>
-          - Currently assigned to the College's Engineering Department, I
+          • Currently assigned to the College's Engineering Department, I
           currently teach Structured Programming: Engineering Problem Solving
           with C++.
         </li>
         <li>
-          - Mainly focus on teaching students Problem Solving Approaches, C++
+          • Mainly focus on teaching students Problem Solving Approaches, C++
           Data Structures, and common Coding Algorithms.
         </li>
         <li>
-          - Teaching approach is to first lecture students for roughly 40-60
+          • Teaching approach is to first lecture students for roughly 40-60
           minutes on the topic assigned for that particular class, after which I
           then have students do hands-on Coding Problems.
         </li>
         <li>
-          - The first 1 or 2 Coding Problems are typically written out on the
+          • The first 1 or 2 Coding Problems are typically written out on the
           whiteboard by me, however all of the code is provided by the students
           (open forum type discourse), where I ask students how we should solve
           the problem and whenever they get stuck, I provide leading reasoning
@@ -79,19 +79,19 @@ export default [
     details: (
       <ol>
         <li>
-          - Responsible for the design/coding process for creating Responsive
+          • Responsible for the design/coding process for creating Responsive
           Web Apps/Websites for startup clients in CT.
         </li>
         <li>
-          - Web Apps/Websites varied from single page to multi-page sites and
+          • Web Apps/Websites varied from single page to multi-page sites and
           were typically developed using NextJS as a framework.
         </li>
         <li>
-          - Aside from NextJS, technologies that were utilized in client
+          • Aside from NextJS, technologies that were utilized in client
           projects were TailwindCSS, MongoDB and Git for version control.
         </li>
         <li>
-          - Some clients also provided Figma (Design) Files, which assisted me
+          • Some clients also provided Figma (Design) Files, which assisted me
           in having to design from scratch.
         </li>
       </ol>
@@ -105,17 +105,17 @@ export default [
     details: (
       <ol>
         <li>
-          - Assigned to the College’s Computer Information Systems Department, I
+          • Assigned to the College’s Computer Information Systems Department, I
           taught Programming Logic & Design w/ Python (for 2 semesters) and
           Windows Server Administration (for 1 semester).
         </li>
         <li>
-          - For the Python courses, I focused primarily on teaching students
+          • For the Python courses, I focused primarily on teaching students
           Python Data Structures, Coding Algorithms, and Problem-Solving
           Techniques.
         </li>
         <li>
-          - For Windows Server Administration, I focused primarily on showing
+          • For Windows Server Administration, I focused primarily on showing
           students the various features/tools available in Active Directory, how
           user management could be done via the GUI but also via PowerShell and
           the advantages/disadvantages to both approaches.
@@ -131,17 +131,17 @@ export default [
     details: (
       <ol>
         <li>
-          - Assigned to the College’s Engineering Department, I taught
+          • Assigned to the College’s Engineering Department, I taught
           Structured Programming: Engineering Problem Solving with C++. (for 1
           semester).
         </li>
         <li>
-          - My main focus was to teach students Problem Solving Approaches, C++
+          • My main focus was to teach students Problem Solving Approaches, C++
           Data Structures, and common Coding Algorithms to help them understand
           how to solve a wide variety of coding problems.
         </li>
         <li>
-          - Lectures were given for the first part of class, and then students
+          • Lectures were given for the first part of class, and then students
           were given the rest of the class time to solve the provided Coding
           Packet Problems (some of which we completed together as a class on the
           whiteboard).
@@ -157,17 +157,17 @@ export default [
     details: (
       <ol>
         <li>
-          - Due to COVID-19 and personal health issues, I took time off from my
+          • Due to COVID-19 and personal health issues, I took time off from my
           professional career and decided to continue my formal education by
           enrolling at UMGC to further enhance my knowledge-base in the field of
           Cybersecurity.
         </li>
         <li>
-          - It was here that I was able to obtain a better understanding of
+          • It was here that I was able to obtain a better understanding of
           Cybersecurity Frameworks, such as those provided by NIST.
         </li>
         <li>
-          - Also gained a better understanding of the role cryptography plays in
+          • Also gained a better understanding of the role cryptography plays in
           todays world, and the struggles of jurisdiction when it comes to
           international cyberlaw, among many other topics!
         </li>
@@ -182,32 +182,32 @@ export default [
     details: (
       <ol>
         <li>
-          - Responsible for the management of three small/medium sized client
+          • Responsible for the management of three small/medium sized client
           companies’ IT network infrastructure (CT based clients).
         </li>
         <li>
-          - Utilized Hypervisor software (vSphere Hypervisor) to manage client
+          • Utilized Hypervisor software (vSphere Hypervisor) to manage client
           Linux (CentOS 7) and Windows Servers.
         </li>
         <li>
-          - Managed Microsoft Azure Active Directory for clients who had cloud
+          • Managed Microsoft Azure Active Directory for clients who had cloud
           environments but also had to manage situations in which client’s had
           hybrid (on-prem and cloud) environments.
         </li>
         <li>
-          - Regularly provided maintenance and support for client Data Backups
+          • Regularly provided maintenance and support for client Data Backups
           via Datto's cloud console manager.
         </li>
         <li>
-          - Clients were using Datto Siris devices which held local and cloud
+          • Clients were using Datto Siris devices which held local and cloud
           backups of client data.
         </li>
         <li>
-          - Setup and maintained client WLANs, this included mapping out best
+          • Setup and maintained client WLANs, this included mapping out best
           locations for Wireless APs.
         </li>
         <li>
-          - Utilized ConnectWise as the company ticketing system to manage
+          • Utilized ConnectWise as the company ticketing system to manage
           client tickets, clock billable hours, and store client documentation.
         </li>
       </ol>
@@ -221,24 +221,24 @@ export default [
     details: (
       <ol>
         <li>
-          - Implemented the district's first WDS server to help streamline
+          • Implemented the district's first WDS server to help streamline
           staff/student PC/laptop image deployment.
         </li>
         <li>
-          - Wrote Bash and PowerShell scripts, which I included within the WDS
+          • Wrote Bash and PowerShell scripts, which I included within the WDS
           server configuration to help automate certain customizations for
           specific software/Windows settings.
         </li>
         <li>
-          - Assisted in the management of district Active Directory,
+          • Assisted in the management of district Active Directory,
           PowerSchool, Google Admin Console, and MDM Solution (JAMF).
         </li>
         <li>
-          - Assisted the Network Manager in configuring and installing district
+          • Assisted the Network Manager in configuring and installing district
           L3 Juniper, and Cisco Switches.
         </li>
         <li>
-          - Provided in person/remote technical assistance to district staff.
+          • Provided in person/remote technical assistance to district staff.
         </li>
       </ol>
     ),
@@ -251,24 +251,24 @@ export default [
     details: (
       <ol>
         <li>
-          - Assigned as the Lead Admin for the district's MDM solution
+          • Assigned as the Lead Admin for the district's MDM solution
           TechPilot, which managed over 800 active IoT devices.
         </li>
         <li>
-          - Responsible for the management of two Level I technicians, this
+          • Responsible for the management of two Level I technicians, this
           included assigning them daily support tickets and acting as a resource
           to them when they needed help resolving a support ticket.
         </li>
         <li>
-          - Assisted in the management of district Active Directory,
+          • Assisted in the management of district Active Directory,
           PowerSchool, and Google Admin Console.
         </li>
         <li>
-          - Assisted the Network Manager with the configuration and deployment
+          • Assisted the Network Manager with the configuration and deployment
           of district Cisco L3 Switches.
         </li>
         <li>
-          - Provided in person/remote technical assistance to district staff.
+          • Provided in person/remote technical assistance to district staff.
         </li>
       </ol>
     ),
@@ -281,11 +281,11 @@ export default [
     details: (
       <ol>
         <li>
-          - Pursued the knowledge that a Cyber Security Systems degree had to
+          • Pursued the knowledge that a Cyber Security Systems degree had to
           offer.
         </li>
         <li>
-          - Learned about Coding Data Structures, SQL Databases, Linux Server
+          • Learned about Coding Data Structures, SQL Databases, Linux Server
           Administration (CentOS), IT Networking, Digital Forensics, Cyber
           Security theory/hands-on practice, among many other topics during my
           time at St. John's University.
